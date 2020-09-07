6e15d036bad5b07a93d3f30ba0112d6a5a22b5c1
andrjm <andrjm@uio.no>
2020-09-07 15:33:26 +0200

    skrevet inn LU decomposition

--- Prosjekt_1/project1.cpp
+++ Prosjekt_1/project1.cpp
@@ -2,24 +2,84 @@
 #include <iostream>   // input and output
 #include <cmath>      // math functions
 #include "time.h"
+#include <new>
+#include <cstdio>
+#include <cstring>
+#include "lib.h"
 using namespace std;
 
-// Defining exact solution:
+
+// Defining exact solution U(x) to the differential equation
 double U(double x)
 {
   return 1 - (1 - exp(-10))*x - exp(-10*x);
 }
-// Defining RHS:
+
+
+// Defining RHS of differential equation
 double f(double x)
 {
-
   return 100*exp(-10*x);
-
 }
+
+
+// Defining function for LU decomposition, finding inverse of matrix
+void inverse(int n, double **matr)
+{
+int *index;
+double d, *col, **y;
+
+index = new int[n];
+col  = new double[n];
+
+ //for (int i = 0; i < n; i++) {
+   //for (int j = 0; j< n; j++) {
+     //cout << matr[i][j] << " ";
+   //}
+  // cout << endl;
+ //}
+
+y = (double **) matrix(n, n, sizeof(double));
+
+ludcmp(matr, n, index, &d);   // LU decompose  a[][]
+
+// find inverse of a[][] by columns
+for(int j = 0; j < n; j++) {
+
+// initialize right-side of linear equations
+for(int i = 0; i < n; i++) col[i] = 0.0;
+col[j] = 1.0;
+
+lubksb(matr, n, index, col);
+
+// save result in y[][]
+for(int i = 0; i < n; i++) y[i][j] = col[i];
+}   //j-loop over columns
+
+// return the inverse matrix in a[][]
+for(int i = 0; i < n; i++) {
+  for(int j = 0; j < n; j++) matr[i][j] = y[i][j];
+}
+
+//for (int i = 0; i < n; i++) {
+  //for (int j = 0; j< n; j++) {
+    //cout << matr[i][j] << " ";
+  //}
+  //cout << endl;
+//}
+
+free_matrix((void **) y);     // release local memory
+delete [] col;
+delete []index;
+}  // End: function inverse()
+
+
+
+
+
 
 int main (int argc, char** argv)
 {
-
   int n = pow(10, atof(argv[1]));
   double *x = new double[n];
   double *y = new double[n];
@@ -31,6 +91,7 @@ int main (int argc, char** argv)
   }
 
 int arg = atoi(argv[2]);
+
 
 // THE GENERAL CASE
 if (arg == 0)
@@ -67,12 +128,11 @@ for (int i = 0; i < n; i++)
   b_tilde[0] = b[0];
   g_tilde[0] = g[0];
 
-
 // Timing the Gauss elimination for the general case
-    clock_t start, finish;  //  declare start and final time
+    clock_t start, finish;
     start = clock();
 
-// Forward substitution, general case:
+// Forward substitution, general case
   for (int i = 0; i < n-1; i++)
   {
     b_tilde[i+1] = b[i+1] - a[i]*c[i]/b_tilde[i];
@@ -83,22 +143,27 @@ for (int i = 0; i < n; i++)
   double *v = new double[n];
   v[-1] = g_tilde[-1]/b_tilde[-1];
 
-  // Backward substitution, general case:
+  // Backward substitution, general case
   for (int j = n-1; j > 0; j--)
   {
     v[j-1] = (g_tilde[j-1] - c[j-1]*v[j])/b_tilde[j-1];
   }
 
+// Ending timer
   finish = clock();
   double time = (double (finish - start)/CLOCKS_PER_SEC);
 
 
+// Output of x and y values
   cout << n << endl;
   for (int i = 0; i < n; i++)
   {
     cout << x[i] << " " << v[i] << endl;
   }
+
+// Printing time for substitution
   cout << "Substitution took " << time << " seconds to execute in the general case." << endl;
+
   delete [] a;
   delete [] b;
   delete [] b_tilde;
@@ -107,6 +172,7 @@ for (int i = 0; i < n; i++)
   delete [] g_tilde;
   delete [] v;
 }
+
 
 
 // THE SPECIAL CASE
@@ -126,30 +192,35 @@ else if (arg == 1)
   clock_t start, finish;  //  declare start and final time
   start = clock();
 
-// Forward substitution, special case:
+// Forward substitution, special case
 for (int i = 1; i < n; i++)
 {
-  y[i] += y[i-1]*i/(i + 1); // Oppdaterer til f_tilde
+  y[i] += y[i-1]*i/(i + 1);
 }
 
 y[n-1] /= (n + 1)/n;
 
-// Backward substitution, special case:
+// Backward substitution, special case
 for (int j = n-2; j > -1; j--)
 {
-  y[j] = (y[j] + y[j+1])*(j)/(j+1); // Oppdaterer til u
+  y[j] = (y[j] + y[j+1])*(j+1)/(j+2);
 }
 
+// Ending timer
 finish = clock();
 double time = (double (finish - start)/CLOCKS_PER_SEC);
 
 
+// Output of x and y values
 cout << n << endl;
 for (int i = 0; i < n; i++)
 {
-  cout << x[i] << " " << y[i] << endl;
+  //cout << x[i] << " " << y[i] << endl;
 }
-cout << "Substitution took " << time << " seconds to execute in the special case." << endl;
+
+// Printing time for substitution
+//cout << "Substitution took " << time << " seconds to execute in the special case." << endl;
+
 
 
 // Computing the relative error
@@ -172,15 +243,62 @@ for(int i=0; i<n; i++)
       eps_max = eps;
     }
 
-cout << eps_max << endl;
+//cout << eps_max << endl;
 
-delete [] u;
+
+
+// Declaring matrix for LU decomposition
+double **matr;
+
+matr = (double **) matrix(n, n, sizeof(double));
+for(int i = 0; i < n; i++)
+{
+for(int j = 0; j < n; j++)
+{
+  matr[i][j] = 0;
+}
+}
+
+// Defining matrix A
+matr[0][0] = 2;
+for (int i = 1; i < n; i++)
+{
+  matr[i][i] = 2;
+  matr[i-1][i] = -1;
+  matr[i][i-1] = -1;
+ }
+
+// Finding inverse of matrix A
+inverse(n, matr);
+
+double *g = new double[n];
+for (int i=0; i<n; i++)
+{
+  g[i] = pow(h,2)*f(x[i]);
 }
 
 
+// Matrix-vector multiplication A^{-1}*g = v
+double *v = new double[n];
+for (int i = 0; i < n; i++)
+{
+  for (int j = 0; j < n; j++)
+  {
+    v[i] += matr[i][j]*g[j];
+  }
+}
+
+// Output of x and v values for LU decomposition
+for (int i = 0; i < n; i++)
+{
+  cout << x[i] << " " << v[i] << endl;
+}
 delete [] x;
 delete [] y;
-
+delete [] u;
+delete [] v;
+delete [] g;
 
 return 0;
+}
 }
