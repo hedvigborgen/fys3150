2a0cd1eff051162336a461adf4abe2a802e40e52
andrjm <andrjm@uio.no>
2020-09-29 12:26:22 +0200

    Endret mainfilen

--- project2/code/main.cpp
+++ project2/code/main.cpp
@@ -3,36 +3,16 @@
 
 int main(int argc, char *argv[]) {
 
-double rho_max;
-Eigensolver solver;
+  double rho_max;
+  Eigensolver solver;
 
-  if (argc == 2){
-    cout << "Running program for potential V = 0." << endl;
-    solver.m_n = atoi(argv[1]);
-    rho_max = 0;
-    solver.initialize(rho_max);
-    solver.diagonalize(solver.m_A);
-  }
+  cout << "Enter n:" << endl;
+  cin >> solver.m_n;
+  cout << "Enter rho_max:" << endl;
+  cin >> rho_max;
 
-  else if (argc == 3){
-    solver.m_n = atoi(argv[1]);
-    rho_max = atoi(argv[2]);
-    solver.initialize(rho_max);
-    solver.diagonalize(solver.m_A);
-  }
-
-  else {
-    cout << "Number of arguments not valid." << endl;
-    cout << "Enter n:" << endl;
-    cin >> solver.m_n;
-    cout << "Enter rho_max:" << endl;
-    cin >> rho_max;
-
-    solver.initialize(rho_max);
-    solver.diagonalize(solver.m_A);
-    solver.print();
-  }
-
-
+  solver.initialize(rho_max);
+  solver.diagonalize(solver.m_A);
+  solver.print();
   return 0;
 }
