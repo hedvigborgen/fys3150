88879ebf8f3e0660ee19b79132235b0d0f6b7334
kamillasulebakk <kamillasulebakk@gmail.com>
2020-12-14 13:03:31 +0100

    plot for optimal step lenght

--- project5/code/optimalStepLength.py
+++ project5/code/optimalStepLength.py
@@ -15,83 +15,42 @@ equilibrationTime = 100_000
 MCCs = 1_000_000
 charge = 1
 whichMethod = 0
-# filenames = ['../output/EnergyasFunctionofAlpha0.dat', '../output/EnergyasFunctionofAlpha1.dat']
 write = 'at the end'
-steps = np.linspace(0,3,10)
-alpha = np.linspace(0.3, 2.40, 8)
+steps = np.linspace(0.1, 3.5, 10)
+alpha = np.linspace(0.2, 2.30, 8)
 
 
 subprocess.call(['c++', '-std=c++11', '-o', 'main.exe', 'QuantumDot.cpp', 'main.cpp', '-larmadillo', '-O3', '-march=native', '-Xpreprocessor', '-fopenmp', '-lomp'])
 
+
+acceptedChanges = np.zeros((len(alpha), len(steps)))
+
 # Compiling the C++ script
 for i, step in enumerate(steps):
-    subprocess.call(['./main.exe', f'{variations}', f'{alpha0}', f'{deltaAlpha}', f'{equilibrationTime}', f'{MCCs}', f'{charge}', f'{whichMethod}', write, f'{step}'])
+    subprocess.call(['./main.exe', f'{variations}', f'{alpha0}', f'{deltaAlpha}', f'{equilibrationTime}', f'{MCCs}', f'{step}', f'{charge}', f'{whichMethod}', write])
 
-    infile = open(filename, f'../output/EnergyasFunctionofAlpha0_{step}')
+    infile = open(f'../output/EnergyasFunctionofAlpha0_%.2f.dat' %step)
     lines = infile.readlines()
 
-    acceptedChanges_1 = np.zeros(len(lines)-2)
-    acceptedChanges_2 = np.zeros(len(lines)-2)
-    acceptedChanges_3 = np.zeros(len(lines)-2)
-    acceptedChanges_4 = np.zeros(len(lines)-2)
-    acceptedChanges_5 = np.zeros(len(lines)-2)
-    acceptedChanges_6 = np.zeros(len(lines)-2)
-    acceptedChanges_7 = np.zeros(len(lines)-2)
-    acceptedChanges_8 = np.zeros(len(lines)-2)
-
-    for i, line in enumerate(lines):
-        if 2 <= i:
-            acceptedChanges[i-2] = float(vals[3])
+    for j, line in enumerate(lines):
+        vals = line.split()
+        if 2 <= j:
+            acceptedChanges[j-2,i] = float(vals[3])
 
     infile.close()
-    return alpha, expEnergy, expEnergySquared, acceptedChanges
 
 
+colors = ['#BA8BCB', '#FEB144', '#9EE09E', '#1A7DA8', '#FF6663','#F3E448', '#FC89B2', '#8ED3DB']
 
-"""
-    # Defining function to read data files
-    def read_file(filename):
-        infile = open(filename, 'r')
-        lines = infile.readlines()
+# Plotting the variation of the energy
+# as function of MCCs for different values of alpha
+fig, ax = plt.subplots()
+for i, alpha_ in enumerate(alpha):
+    ax.plot(steps, acceptedChanges[i]/MCCs * 100, color=colors[i], label=r'$\alpha$ = %.2f' %alpha_)
 
-        alpha = np.zeros(len(lines)-2)
-        expEnergy = np.zeros(len(lines)-2)
-        expEnergySquared = np.zeros(len(lines)-2)
-        acceptedChanges = np.zeros(len(lines)-2)
-
-        for i, line in enumerate(lines):
-            if 2 <= i:
-                vals = line.split()
-                alpha[i-2] = float(vals[0])
-                expEnergy[i-2] = float(vals[1])
-                expEnergySquared[i-2] = float(vals[2])
-                acceptedChanges[i-2] = float(vals[3])
-        infile.close()
-        return alpha, expEnergy, expEnergySquared, acceptedChanges
-
-    alpha0, expEnergy0, expEnergySquared0, acceptedChanges1 = read_file(filenames[0])
-    alpha1, expEnergy1, expEnergySquared1, acceptedChanges2 = read_file(filenames[1])
-
-    # Plotting
-    fig, ax = plt.subplots()
-    ax.plot(alpha1, expEnergy1, color=colors[1], label='With Coulomb interaction')
-    ax.plot(alpha0, expEnergy0, color=colors[0], label='Without Coulomb interaction')
-    ax.set_title(r'Expectation value of the energy as function of $\alpha$', fontsize=20)
-    ax.set_xlabel(r'$\alpha$', fontsize=15)
-    ax.set_ylabel(r'$E$ [J]', fontsize=15)
-    ax.legend(fontsize=15)
-    fig.savefig(f'../plots/energyasfunctionofalpha.pdf')
-
-    # Plotting the variation of the energy
-    # as function of MCCs for different values of alpha
-    variance0 = expEnergySquared0 - expEnergy0**2
-    variance1 = expEnergySquared1 - expEnergy1**2
-    fig, ax = plt.subplots()
-    ax.plot(alpha0, variance0, color=colors[1], label='With Coulomb interaction')
-    ax.plot(alpha1, variance1, color=colors[0], label='Without Coulomb interaction')
-    ax.set_title(r'Variance in energy as function of $\alpha$', fontsize=20)
-    ax.set_xlabel(r'$\alpha$', fontsize=15)
-    ax.set_ylabel(r'$\sigma$ [J]', fontsize=15)
-    ax.legend(fontsize=15)
-    fig.savefig(f'../plots/variationasfunctionofalpha.pdf')
-"""
+ax.plot(steps, np.ones(len(steps))*50, ':', color = '#000000')
+ax.set_title(r'Percentage of accepted changes as function of step length', fontsize=20)
+ax.set_xlabel('Accepted changes', fontsize=15)
+ax.set_ylabel(r'$\delta$', fontsize=15)
+ax.legend(fontsize=15)
+fig.savefig(f'../plots/optimalStepLength.pdf')
